95452f19a1ea978d7703958ffc5488c8b663cf79
benwhit44 <63593607+benwhit44@users.noreply.github.com>
2023-03-31 13:44:05 -0500

    First working version creates CSV

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import openpyxl
 import urllib.request
 import xml.etree.ElementTree as ET
+import json
+import csv
+import pandas as pd
+# from openpyxl import Workbook
 
 wb = openpyxl.load_workbook('2022 YIR Mailing List.xlsx', data_only=True)
 print(wb.sheetnames)
 sheet = wb['2022 combined list']
-n_rows = 4
+n_rows = 5
 
 # Dynamically create dictionary from the spreadsheet
 address_dict = {}
@@ -36,6 +40,10 @@ for k in address_dict['Addr1']:
 #     x = address_dict.get('Addr1', {}).get('jesus', 'nothing')
 # print(x)
 
+# new_addr1_dict, new_addr2_dict, new_city_dict, new_state_dict, new_zip_dict, new_extzip_dict = {}
+temp_addr1_dict, temp_addr2_dict, temp_city_dict, temp_state_dict\
+    , temp_zip_dict, temp_extzip_dict, temp_err_dict = {}, {}, {}, {}, {}, {}, {}
+
 for k in address_dict['Addr1']:
     requestXML = """
     <?xml version="1.0"?>
@@ -59,7 +67,7 @@ for k in address_dict['Addr1']:
     docString = urllib.parse.quote_plus(docString)
 
     url = "http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=" + docString
-    print(url + "\n\n")
+    # print(url + "\n\n")
 
     response = urllib.request.urlopen(url)
     if response.getcode() != 200:
@@ -68,79 +76,54 @@ for k in address_dict['Addr1']:
         exit()
 
     contents = response.read()
-    print(contents)
+    # print(contents)
 
-# root = ET.fromstring(contents)
-# for address in root.findall('Address'):
-#     print()
-#     print("Address1: " + address.find("Address1").text)
-#     print("Address2: " + address.find("Address2").text)
-#     print("City:	 " + address.find("City").text)
-#     print("State:	" + address.find("State").text)
-#     print("Zip5:	 " + address.find("Zip5").text)
-#     print("Zip4:	 " + address.find("Zip4").text)
+    root = ET.fromstring(contents)
+    # print(root.text)
 
-#########################
+    for address in root.findall('Address'):
+        if not address.find("Error"):
+            # print("Address1: " + address.find("Address1").text)
+            # print("Address2: " + address.find("Address2").text)
+            # print("City:	 " + address.find("City").text)
+            # print("State:	" + address.find("State").text)
+            # print("Zip5:	 " + address.find("Zip5").text)
+            # print("Zip4:	 " + address.find("Zip4").text)
+            temp_addr1_dict[k] = address.find("Address1").text
+            temp_addr2_dict[k] = address.find("Address2").text
+            temp_city_dict[k] = address.find("City").text
+            temp_state_dict[k] = address.find("State").text
+            temp_zip_dict[k] = address.find("Zip5").text
+            temp_extzip_dict[k] = address.find("Zip4").text
+            temp_err_dict[k] = ""
 
-# requestXML = """
-# <?xml version="1.0"?>
-# <AddressValidateRequest USERID="292BENWA3717">
-#     <Revision>1</Revision>
-#     <Address ID="0">
-#         <Address1>2335 S State</Address1>
-#         <Address2>Suite 300</Address2>
-#         <City>Provo</City>
-#         <State>UT</State>
-#         <Zip5>84604</Zip5>
-#         <Zip4/>
-#     </Address>
-# </AddressValidateRequest>
-# """
-#
-# #prepare xml string doc for query string
-# docString = requestXML
-# docString = docString.replace('\n','').replace('\t','')
-# docString = urllib.parse.quote_plus(docString)
-#
-# url = "http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=" + docString
-# print(url + "\n\n")
-#
-# response = urllib.request.urlopen(url)
-# if response.getcode() != 200:
-#     print("Error making HTTP call:")
-#     print(response.info())
-#     exit()
-#
-# contents = response.read()
-# print(contents)
-#
-# root = ET.fromstring(contents)
-# for address in root.findall('Address'):
-#     print()
-#     print("Address1: " + address.find("Address1").text)
-#     print("Address2: " + address.find("Address2").text)
-#     print("City:	 " + address.find("City").text)
-#     print("State:	" + address.find("State").text)
-#     print("Zip5:	 " + address.find("Zip5").text)
-#     print("Zip4:	 " + address.find("Zip4").text)
+        else:
+            temp_addr1_dict[k] = ""
+            temp_addr2_dict[k] = ""
+            temp_city_dict[k] = ""
+            temp_state_dict[k] = ""
+            temp_zip_dict[k] = ""
+            temp_extzip_dict[k] = ""
+            for err in root.findall('Address/Error'):
+                temp_err_dict[k] = err.find("Description").text
+                # print("Error: ", err.find("Description").text)
+        #     print("get error: ", root.text)
 
+new_addr1_dict = {"New_Addr1": temp_addr1_dict}
+new_addr2_dict = {"New_Addr2": temp_addr2_dict}
+new_city_dict = {"New_City": temp_city_dict}
+new_state_dict = {"New_State": temp_state_dict}
+new_zip_dict = {"New_Zip": temp_zip_dict}
+new_extzip_dict = {"New_ExtZip": temp_extzip_dict}
+new_err_dict = {"Error_Description" : temp_err_dict}
 
+address_dict = address_dict | new_addr1_dict | new_addr2_dict | new_city_dict\
+               | new_state_dict | new_zip_dict | new_extzip_dict | new_err_dict
+list_dict = list(address_dict)
+# print(list_dict)
+address_json = json.dumps(address_dict, indent=4)
+print(address_json)
 
+df = pd.read_json(address_json)
 
-
-####################################
-# import pandas as pd
-# from geopy.geocoders import Nominatim
-# # messy_address = pd.read_excel("Book1.xlsx")
-# # messy_address = pd.read_excel("2022 YIR Mailing List.xlsx", sheet_name="2022 combined list")
-# messy_address = pd.read_csv("2022 YIR Mailing List.csv")
-# geolocator = Nominatim(user_agent="benwhit44@gmail.com")
-#
-#
-# def extract_clean_address(address):
-#     try:
-#         location = geolocator.geocode(address)
-#         return location.address
-#     except:
-#         return ''
-# messy_address['clean address'] = messy_address.apply(lambda x: extract_clean_address(x['Raw Address']) , axis =1  )
+df.to_csv('clean_addresses.csv', encoding='utf-8', index=False)
